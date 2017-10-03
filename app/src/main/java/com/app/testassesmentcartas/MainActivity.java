21b988bd2e983e32af40aece192c985461920ea2
Yacov Rosenberg <yacov94@yahoo.com>
2017-10-03 12:03:34 -0300

    fix image, add istouchLinear

--- app/src/main/java/com/app/testassesmentcartas/MainActivity.java
+++ app/src/main/java/com/app/testassesmentcartas/MainActivity.java
@@ -7,12 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout linearLayout;
     ImageButton card;
     TextView msg;
     int[] imagem = {
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        linearLayout = (LinearLayout) findViewById(R.id.linear);
         card = (ImageButton) findViewById(R.id.card);
         msg = (TextView) findViewById(R.id.message);
         btnAgain = (Button) findViewById(R.id.btnAgain);
@@ -42,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
         Random random = new Random();
         int ran =  random.nextInt(imagem.length);
+        boolean win = random.nextBoolean();
         this.card = (ImageButton) card;
         this.card.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), imagem[ran]));
-
         if (imagem[ran] == R.drawable.carta){
             msg.setText("Você venceu");
             msg.setVisibility(View.VISIBLE);
@@ -56,13 +59,26 @@ public class MainActivity extends AppCompatActivity {
             btnAgain.setVisibility(View.VISIBLE);
         }
 
+        isLinearlayoutTouchEnabled(false);
 
+
+
+
+    }
+
+    private void isLinearlayoutTouchEnabled(Boolean condition){
+        for(int i = 0; i<linearLayout.getChildCount(); i++ ){
+            View view = linearLayout.getChildAt(i);
+
+            view.setEnabled(condition);
+        }
     }
 
     public void againClick(View view){
         card.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.choice));
         msg.setVisibility(View.INVISIBLE);
         btnAgain.setVisibility(View.INVISIBLE);
+        isLinearlayoutTouchEnabled(true);
     }
 }
 
